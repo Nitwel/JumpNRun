d9213918e1f92f4a606a20eae5d8ede2fc2192c0
Nitwel <nitwel@arcor.de>
2022-02-26 17:50:50 +0100

    add simple tilemap

--- Player.gd
+++ Player.gd
@@ -34,5 +34,5 @@ func _physics_process(delta):
 func get_move_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
+		-Input.get_action_strength("move_up") if is_on_floor() and Input.is_action_just_pressed("move_up") else 0.0
 	)
